87b56673916ba0ba1a8c38de135d3ae1c541addf
NikhilMarewad <nikhilmarewad@gmail.com>
2019-12-07 15:36:26 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,21 @@
 // Include form validation functions here
+function validateForm(){
+	
+	var txt=document.forms["myForm"]["name1"].value;
+	var no=document.forms["myForm"]["number1"].value;
+	var dor=document.forms["myForm"]["dates1"].value;
+	if (txt == "") {
+	    alert("Title must be filled out");
+        return false;		
+	 } else if (no == "") {
+	    alert("Number must be filled out");
+		return false;
+	 } else if (dor == "") {
+	    alert("Date must be filled out");
+		return false;
+	 } else {
+		return true;
+	 }
+	
+	
+}
